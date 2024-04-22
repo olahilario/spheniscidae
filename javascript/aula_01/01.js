e8e95423bfe8923f8414487c15bfa70cb368934c
Pedro Hilário <grandejacare@gmail.com>
2024-04-22 17:01:36 -0300

    Add files via upload

---
+++ javascript/aula_01/01.js
@@ -0,0 +1,16 @@
+// comentario em javascript //
+// change tab identation in javascript: 2
+
+let nome = 'Pedro Hilário' // string
+let idade = 35 // int
+let altura = 1.90 // float
+let brasileiro = true // contante booleana
+
+console.log(nome)
+console.log(idade)
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, ${altura} de altura.`)
+
+console.log(typeof(brasileiro))
+
+
